613247f7151f43ea4dac980c3bef21afd1c7f48b
alvinyanson <a.yanson@arcanys.com>
2024-08-01 11:58:45 +0800

    writing custom middleware and creating extension

--- MiddlewareDemo]/Program.cs
+++ MiddlewareDemo]/Program.cs
@@ -1,110 +1,19 @@
+using MiddlewareDemo_;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//app.Use(async (context, next) =>
-//{
-//    // Do work that can write to the Response.
-//    Console.WriteLine("Before1");
-//    await next.Invoke();
-//    Console.WriteLine("After1");
-//    // Do logging or other work that doesn't write to the Response.
-//});
+app.UseHttpsRedirection();
 
-//app.Use(async (context, next) =>
-//{
-//    // Do work that can write to the Response.
-//    Console.WriteLine("Before2");
-//    await next.Invoke();
-//    Console.WriteLine("After2");
-//    // Do logging or other work that doesn't write to the Response.
-//});
+app.UseRequestCulture();
 
-//app.Use(async (context, next) =>
-//{
-//    // Do work that can write to the Response.
-//    Console.WriteLine("Before3");
-//    await next.Invoke();
-//    Console.WriteLine("After3");
-//    // Do logging or other work that doesn't write to the Response.
-//});
-
-//app.Run(async context =>
-//{
-//    await context.Response.WriteAsync("Hello from 2nd delegate.");
-//});
-
-
-//app.Run();
-
-
-////This will not get called
-//app.Run(async context =>
-//{
-//    await context.Response.WriteAsync("Hello from 2nd delegate.");
-//});
-
-
-//output
-//Before1
-//Before2
-//Before3
-//Before3
-//Before2
-//Before1
-
-
-//app.UseWhen(context => context.Request.Query.ContainsKey("branch"),
-//    appBuilder => HandleBranchAndRejoin(appBuilder));
-
-//app.Run(async context =>
-//{
-//    await context.Response.WriteAsync("Hello from non-Map delegate.");
-//});
-
-//app.Run();
-
-//void HandleBranchAndRejoin(IApplicationBuilder app)
-//{
-//    var logger = app.ApplicationServices.GetRequiredService<ILogger<Program>>();
-
-//    app.Use(async (context, next) =>
-//    {
-//        var branchVer = context.Request.Query["branch"];
-//        logger.LogInformation("Branch used = {branchVer}", branchVer);
-
-//        // Do work that doesn't write to the Response.
-//        await next();
-//        // Do other work that doesn't write to the Response.
-//    });
-//}
-
-
-app.Map("/map1", HandleMapTest1);
-
-app.Map("/map2", HandleMapTest2);
-
-app.Run(async context =>
+app.Run(async (context) =>
 {
-    await context.Response.WriteAsync("Hello from non-Map delegate.");
+    await context.Response.WriteAsync(
+        $"CurrentCulture.DisplayName: {CultureInfo.CurrentCulture.DisplayName}");
 });
 
 app.Run();
-
-static void HandleMapTest1(IApplicationBuilder app)
-{
-    app.Run(async context =>
-    {
-        await context.Response.WriteAsync("Map Test 1");
-    });
-}
-
-static void HandleMapTest2(IApplicationBuilder app)
-{
-    app.Run(async context =>
-    {
-        await context.Response.WriteAsync("Map Test 2");
-    });
-}
-
 
 
---
+++ MiddlewareDemo]/RequestCultureMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace MiddlewareDemo_
+{
+    public static class RequestCultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestCulture(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestCultureMiddleware>();
+        }
+    }
+}
